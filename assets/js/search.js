fb3f93902a7989475e651554f6943ee11f45581f
James P. Howard, II <jh@jameshoward.us>
2024-09-23 21:46:41 -0400

    All new local search

---
+++ assets/js/search.js
@@ -0,0 +1,88 @@
+(e => {
+    const t = e.querySelector("#search-input"),
+        n = e.querySelector(".search-results");
+    if (!t || !n) return;
+    const i = t.dataset;
+    let o;
+
+    // Helper function to get the value of a query parameter from the URL
+    function getQueryParam(param) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);
+    }
+
+    // Get the "q" parameter from the URL and prepopulate the search input field
+    const queryParam = getQueryParam("q");
+    if (queryParam) {
+        t.value = queryParam;  // Prepopulate the search input with the value from "q"
+    }
+
+    function r(e, t, n) {
+        let i;
+        for (let n of e.matches) n.key === t && (i = n.indices);
+        const o = e.item[t];
+        if (!i) return o.substr(0, n);
+        let r, s, l = 0,
+            c = Math.ceil(n / 2);
+        for (; s = i.shift();) s[1] - s[0] >= l && (r = s, l = r[1] - r[0]);
+        return (r[0] - c > 0 ? "[...] " : "") + o.substring(r[0] - c, r[0]) + "<b>" + o.substring(r[0], r[1] + 1) + "</b>" + o.substring(r[1] + 1, r[1] + 1 + c) + (r[1] + 1 + c < o.length ? " [...] " : "");
+    }
+
+    function triggerSearch() {
+        if (queryParam) {
+            u();  // Trigger the search with the prefilled query
+        }
+    }
+
+    t.addEventListener("focus", (e => {
+        if (o) return;
+        t.placeholder = i.infoInit || "Loading search index... Please hold on.";
+        const n = new XMLHttpRequest;
+        n.responseType = "json", n.addEventListener("load", (e => {
+            const r = n.response;
+            r && 0 !== r.length ? (t.placeholder = i.infoOk || "Type to search:", o = new Fuse(r, {
+                keys: [{
+                    name: "title",
+                    weight: 5
+                }, "content"],
+                useExtendedSearch: !0,
+                includeMatches: !0,
+                ignoreLocation: !0,
+                threshold: .1
+            }), triggerSearch()) : t.placeholder = i.infoFail || "Failed to load search index!";
+        }), !1), n.open("GET", i.indexUrl || "/data/search.json"), n.send(null);
+    }));
+
+    const s = i.textLength || 300,
+        l = i.limit || 50,
+        c = i.delay || 500,
+        a = n.firstElementChild.cloneNode(!0);
+
+    function u() {
+        if (o) {
+            n.innerHTML = "";
+            for (let e of o.search(t.value, {
+                    limit: l
+                })) {
+                const t = a.cloneNode(!0),
+                    i = t.querySelector("a");
+              i.href = e.item.uri, i.innerHTML = r(e, "title", s), t.querySelector(".search-date").innerHTML = r(e, "date", s), t.querySelector(".search-preview").innerHTML = r(e, "content", s), n.appendChild(t);
+            }
+        }
+    }
+
+    n.innerHTML = "";
+    const d = /Mobi/i.test(navigator.userAgent);
+    t.addEventListener(d ? "change" : "input", d ? u : function(e, t) {
+        let n;
+        return function(...i) {
+            clearTimeout(n), n = setTimeout((() => e(...i)), t);
+        }
+    }(u, c));
+
+    // Automatically trigger focus on the search input if "q" parameter exists
+    if (queryParam) {
+        t.dispatchEvent(new Event('focus'));  // Trigger the focus event to load the index
+    }
+
+})(document);
--- search.md
+++ search.md
@@ -4,11 +4,33 @@ title: Search
 description: Search jameshoward.us
 ---
 
-<div id="powered-by" style="float: right; margin-top: -1.75rem;"></div>
-<div class="ais-InstantSearch">
-   <div id="searchbox" class="ais-SearchBox"></div>
-   <div id="hits"></div>
-   <div id="pagination"></div>
-</div>
+<style type="text/css">
+.main {
+  width: 100%;
+}
+#search-input {
+  width: 100%;
+  font-size: 1.2em;
+  padding: .5em;
+}
+.search-results b {
+  background-color: #f2daa6;
+}
+.search-preview {
+  margin-bottom: 2em;
+}
 
-{% include algolia.html %}
+.single .main a, .single .main h2 {
+  border-bottom: none;
+}
+</style>
+<input type="search" id="search-input">
+<div class="search-results">
+<section>
+<h3 class="toc-line"><a target="_blank"></a><span class="dots"></span><span class="page-num small"></span></h3>
+<h5 class="search-date"></h5>
+<div class="search-preview"></div>
+</section>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/fuse.js@6.6.2" defer></script>
+<script src="/assets/js/search.js" defer></script>
